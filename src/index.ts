c2c61744e3299fd34b773a720f5c8d26b6efd636
unknown <henrytrust1111@gmail.com>
2026-01-05 10:17:28 +0100

    fixed the cors

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   credentials: true,
+// }));
+app.use(cors());
 app.use(express.json());
 
 // Database Connection (use cached connection helper)
